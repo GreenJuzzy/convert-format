cfe664a18c37a0eafd990077bc0120dc5784a8f8
GreenJuzzy <65908209+GreenJuzzy@users.noreply.github.com>
2023-02-15 20:59:28 +0100

    initialize

---
+++ index.js
@@ -0,0 +1,63 @@
+var fs = require("fs");
+var PATH = require("path");
+
+var time = new Date()
+var filesConverted = 0
+
+var fileTypes = ["ts", "js", "mcfunction", "mcstructure", "txt", "json"]
+
+
+if (!fs.existsSync(__dirname + "\\output")) fs.mkdirSync(__dirname + "\\output")
+
+var lookInFolder = async (path = `${__dirname}\\input`) => {
+    var entries = fs.readdirSync(path, { withFileTypes: true });
+
+    var folders = entries.filter(file => file.isDirectory());
+    var files = entries.filter(file => file.isFile()).filter(file => fileTypes.some((type) => file.name.endsWith(type)) == true);
+
+
+    folders.forEach((folder) => {
+        try {
+            fs.mkdirSync(`${path.replace("input", "output")}\\${folder.name}`)
+        } catch (e) { }
+
+        lookInFolder(PATH.join(path, folder.name));
+    });
+
+
+    for (let i = 0; i < files.length; i++) {
+        var fileName = files[i].name
+        var fileData = fs.readFileSync(`${path}\\${fileName}`, "utf8")
+
+
+        var converted = await convert(fileData)
+        filesConverted++
+
+        fs.writeFileSync(`${path.replace("input", "output")}\\${fileName}`, converted)
+
+
+    }
+};
+
+var convert = async (data) => new Promise((resolve, reject) => {
+    data = data.split("\n")
+
+    data.forEach((line, lineNumber) => {
+        var matching = line.match(/execute\s(@s|@e|@r|@p|@a)(((\[.*?)\])?)(~| ~|){3}/g)
+        if (!matching) return;
+
+        matching.forEach((value, matchNumber) => {
+            var selector = value.match(/(@s|@e|@r|@p|@a)/)[0]
+            var options = value.match(/\[.*?\]/g)
+
+            data[lineNumber] = data[lineNumber].replace(value, `execute as ${selector}${options ?? ""} run`)
+        })
+    })
+
+    resolve(data.join("\n"))
+
+})
+
+lookInFolder().then(() => {
+    console.log(`Converted ${filesConverted} files in ${new Date() - time}ms`)
+})
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Convert to 1.19 Format
+
+---
+Simply put your files/folders into the `input` folder and run `index.js` to convert the files. The converted files will be located in the `output` folder.
